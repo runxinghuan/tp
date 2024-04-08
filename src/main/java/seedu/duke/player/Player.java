8c865fbe47c6a25d3cf1fa516f55b5bf368e5187
Incy <85068563+IncyBot@users.noreply.github.com>
2024-04-08 22:58:05 +0800

    Added ability to create teams

--- src/main/java/seedu/duke/player/Player.java
+++ src/main/java/seedu/duke/player/Player.java
@@ -1,8 +1,12 @@
 package seedu.duke.player;
 
-import java.util.Random;
+import seedu.duke.Formatter;
+import seedu.duke.ui.Ui;
 
-//@@author HenryGan138
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Player {
     public String name;
@@ -14,7 +18,7 @@ public class Player {
     public Player(String name, int matchCount) {
         this.name = name;
         this.matchCount = matchCount;
-        this.power=1;
+        this.power = 1;
     }
 
     public Player(String name, int matchCount, int power) {
@@ -23,10 +27,10 @@ public class Player {
         this.power = power;
     }
 
-    public void printSelfInfo(){
+    public void printSelfInfo() {
         System.out.println("--------Player Info--------");
-        System.out.println("Name: "+this.name);
-        System.out.println("Number of match played: "+this.matchCount);
+        System.out.println("Name: " + this.name);
+        System.out.println("Number of match played: " + this.matchCount);
         printPower();
         printSkill();
         System.out.println("---------------------------");
@@ -36,41 +40,98 @@ public class Player {
         return skill;
     }
 
-    protected void printPower(){} //For override
+    protected void printPower() {
+    } //For override
 
-    protected void printSkill(){} //For override
+    protected void printSkill() {
+    } //For override
 
-    public void printGoalBeforeShoot(){} //For override
+    public void printGoalBeforeShoot() {
+    } //For override
 
-    public void printGoalBeforeSave(){} //For override
+    public void printGoalBeforeSave() {
+    } //For override
 
-    public void printGoalAfterShoot(boolean goalScored, int direction){} //For override
+    public void printGoalAfterShoot(boolean goalScored, int direction) {
+    } //For override
 
-    public void upgradePower(int level){} //For override
+    public void upgradePower(int level) {
+    } //For override
 
-    public float aiDirectionAdjust(int aiDir){
+    public float aiDirectionAdjust(int aiDir) {
         return 0;
     } //For override
 
-    public float shootDirectionAdjust(int dir){
+    public float shootDirectionAdjust(int dir) {
         return 0;
     }  //For override
 
-    public float shootDirectionFormula(int left, int right, int dir, int power){
-        float prob=rand.nextFloat();
-        if (power==1){
-            prob=prob/2;
-            return (float) (left*prob+dir*0.5+right*(0.5-prob));
-        }else if (power==2){
-            prob=prob/4;
-            return (float) (left*prob+dir*0.75+right*(0.25-prob));
-        }else{
-            prob=prob/10;
-            return (float)(left*prob+dir*0.9+right*(0.1-prob));
+    public float shootDirectionFormula(int left, int right, int dir, int power) {
+        float prob = rand.nextFloat();
+        if (power == 1) {
+            prob = prob / 2;
+            return (float) (left * prob + dir * 0.5 + right * (0.5 - prob));
+        } else if (power == 2) {
+            prob = prob / 4;
+            return (float) (left * prob + dir * 0.75 + right * (0.25 - prob));
+        } else {
+            prob = prob / 10;
+            return (float) (left * prob + dir * 0.9 + right * (0.1 - prob));
         }
     }
 
-    public float rangeAdjust(){
+    public float rangeAdjust() {
         return 0;
+    }
+
+    // New method to create a custom team
+    public static List<Player> createCustomTeam() {
+        List<Player> customTeam = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Create your custom team:");
+        System.out.println("Enter the number of players in your team (1-11):");
+        int numPlayers = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        if (numPlayers < 1 || numPlayers > 11) {
+            System.out.println("Invalid number of players. Using default team.");
+            return customTeam;
+        }
+
+        for (int i = 1; i <= numPlayers; i++) {
+            System.out.println("Enter the name of player " + i + ":");
+            String playerName = scanner.nextLine();
+
+            System.out.println("Enter the skill level of player " + i + " (1-Beginner, 2-Medium, 3-Expert):");
+            int skillLevel = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            if (skillLevel < 1 || skillLevel > 3) {
+                System.out.println("Invalid skill level. Using default skill level (Beginner).");
+                skillLevel = 1;
+            }
+
+            Player player;
+            switch (skillLevel) {
+                case 1:
+                    player = new BeginnerSkillPlayer(playerName, 0);
+                    break;
+                case 2:
+                    player = new MediumSkillPlayer(playerName, 0);
+                    break;
+                case 3:
+                    player = new ExpertSkillPlayer(playerName, 0);
+                    break;
+                default:
+                    player = new BeginnerSkillPlayer(playerName, 0);
+                    break;
+            }
+
+            customTeam.add(player);
+        }
+
+        System.out.println("Custom team created successfully!");
+        return customTeam;
     }
 }
